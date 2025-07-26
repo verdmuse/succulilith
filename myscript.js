6596ed0dfd07640681fc21645cf29b9334394f05
verdmuse <verdmuse@protonmail.com>
2025-07-26 23:53:03 +0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 let body = document.querySelector("body");
 let p = document.createElement("p");
-let p1 = document.querySelector("p");
+let p1 = document.querySelector("img");
 p.textContent = "Try";
 
 body.insertBefore(p, p1);
