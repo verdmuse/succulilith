14fdeafb960a3fbbab57a5a499d6a4b7c130e065
verdmuse <verdmuse@protonmail.com>
2025-07-26 23:43:05 +0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,5 +1,6 @@
 let body = document.querySelector("body");
 let p = document.createElement("p");
+let p1 = document.querySelector("p");
 p.textContent = "Try";
 
-body.appendChild(p);
+body.insertBefore(p, p1);
