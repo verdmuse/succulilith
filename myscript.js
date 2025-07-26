b226bac029e755c234bb42d624f8df5c0d6820b7
verdmuse <verdmuse@protonmail.com>
2025-07-27 02:04:01 +0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,5 +3,5 @@ let p = document.createElement("p");
 let p1 = document.querySelector("div");
 p.textContent = "Try";
 
-body.children[1].appendChild(p);
+body.children[0].appendChild(p);
 console.log(p1);
